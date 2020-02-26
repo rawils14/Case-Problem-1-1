2738cc483f5ecde011b4fa7736bd0b76b8cddbbe
rawils14 <61478006+rawils14@users.noreply.github.com>
2020-02-25 19:23:25 -0500

    Add files via upload

---
+++ GreenVilleMotto.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+class GreenvilleMotto
+{
+   static void Main()
+   {
+     WriteLine("************************************");
+     WriteLine("*  The stars shine in Greenville.  *");
+     WriteLine("************************************");
+    // Write your main here.
+    // this should output the main
+   }
+}
